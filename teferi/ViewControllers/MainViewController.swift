4789cdb368a4d52cbccba878fc7003b6b6b5e87e
William Barbosa <william.sb@hotmail.com>
2016-10-19 12:38:26 -0200

    :fire: Removes debug code

--- teferi/ViewControllers/MainViewController.swift
+++ teferi/ViewControllers/MainViewController.swift
@@ -7,7 +7,7 @@ import QuartzCore
 import CoreLocation
 import SnapKit
 
-class MainViewController : UIViewController, MFMailComposeViewControllerDelegate
+class MainViewController : UIViewController
 {
     // MARK: Fields
     private let animationDuration = 0.08
@@ -34,9 +34,7 @@ class MainViewController : UIViewController, MFMailComposeViewControllerDelegate
     private var launchAnim : LaunchAnimationView!
     
     @IBOutlet private weak var icon : UIImageView!
-    @IBOutlet private weak var logButton : UIButton!
     @IBOutlet private weak var titleLabel : UILabel!
-    @IBOutlet private weak var debugView : DebugView!
     @IBOutlet private weak var calendarLabel : UIButton!
     
     func inject(_ locationService: LocationService, _ metricsService: MetricsService, _ persistencyService: PersistencyService, _ settingsService: SettingsService, _ editStateService: EditStateService) -> MainViewController
@@ -57,9 +55,6 @@ class MainViewController : UIViewController, MFMailComposeViewControllerDelegate
         
         //Inject PagerViewController's dependencies
         self.pagerViewController.inject(metricsService, settingsService, persistencyService, editStateService)
-        
-        //Debug screen
-        self.debugView.isHidden = true
         
         //Launch animation
         if !self.skipLoadingAnim
@@ -86,12 +81,6 @@ class MainViewController : UIViewController, MFMailComposeViewControllerDelegate
         self.disposeBag = self.disposeBag ?? DisposeBag()
         
         self.gestureRecognizer = ClosureGestureRecognizer(withClosure: { self.editStateService.notifyEditingEnded() })
-        
-        //DEBUG SCREEN
-        self.locationService
-            .locationObservable
-            .subscribe(onNext: self.debugView.onNewLocation)
-            .addDisposableTo(disposeBag!)
         
         //Edit state
         self.editStateService
@@ -156,46 +145,6 @@ class MainViewController : UIViewController, MFMailComposeViewControllerDelegate
         self.onDateChanged(date: today)
     }
     
-    @IBAction func onSendLogButtonTouchUpInside()
-    {
-        guard MFMailComposeViewController.canSendMail() else
-        {
-            return self.showAlert(withTitle: "Something went wrong :(", message: "You need to set up an email account before sending emails.")
-        }
-        
-        guard let baseURL = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first else
-        {
-            return self.showAlert(withTitle: "Something went wrong :(", message: "We are unable to find the log file.")
-        }
-        
-        let fileURL = baseURL.appendingPathComponent("swiftybeaver.log", isDirectory: false)
-        
-        guard let fileData = try? Data(contentsOf: fileURL) else
-        {
-            return self.showAlert(withTitle: "Something went wrong :(", message: "We are unable to find the log file.")
-        }
-        
-        let mailComposer = MFMailComposeViewController()
-        mailComposer.mailComposeDelegate = self
-        mailComposer.setSubject("Superday tracking log")
-        mailComposer.setMessageBody("The log is attached.", isHTML: false)
-        mailComposer.setToRecipients(["paul@toggl.com", "william@toggl.com"])
-        mailComposer.addAttachmentData(fileData, mimeType: "text/plain", fileName: "superday.log")
-        
-        self.present(mailComposer, animated: true, completion: nil)
-    }
-    
-    @IBAction func onDebugButtonTouchUpInside()
-    {
-        self.debugView.isHidden = !self.debugView.isHidden
-    }
-    
-    // MARK: MFMailComposeViewControllerDelegate implementation
-    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?)
-    {
-        controller.dismiss(animated: true, completion: nil)
-    }
-    
     // MARK: Methods
     func skipLoadingAnimation()
     {
@@ -236,22 +185,10 @@ class MainViewController : UIViewController, MFMailComposeViewControllerDelegate
     
     private func onEditChanged(_ isEditing: Bool)
     {
-        //Disable buttons
-        self.addButton.isUserInteractionEnabled = !isEditing
-        self.logButton.isUserInteractionEnabled = !isEditing
-        self.calendarLabel.isUserInteractionEnabled = !isEditing
-        
         //Close add menu
         self.addButton.close()
         
         //Grey out views
         self.editView.isEditing = isEditing
-    }
-    
-    func showAlert(withTitle title: String, message: String)
-    {
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
-        self.present(alert, animated: true, completion: nil)
     }
 }
