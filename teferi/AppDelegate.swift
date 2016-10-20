e61ec7bcfaafa54631dbc098af94d08d0c57b0b9
William Barbosa <william.sb@hotmail.com>
2016-10-20 05:13:34 -0200

    :chart_with_upwards_trend: Adds first TimeSlot of day (#73)

--- teferi/AppDelegate.swift
+++ teferi/AppDelegate.swift
@@ -69,11 +69,7 @@ class AppDelegate : UIResponder, UIApplicationDelegate
                                       self.editStateService)
         
         if self.settingsService.installDate == nil
-        {
-            //App is running for the first time
-            let firstTimeSlot = TimeSlot()
-            self.persistencyService.addNewTimeSlot(firstTimeSlot)
-            
+        {   
             let storyboard = UIStoryboard(name: "Onboarding", bundle: nil)
             let onboardController = storyboard.instantiateViewController(withIdentifier: "OnboardingPager") as! OnboardingPageViewController
             
