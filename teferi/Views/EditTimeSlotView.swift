1aac334ccdf677d1b9b25b5b72c4b151a43fe811
Paul Scharf <amul@amulware.net>
2016-10-21 07:02:07 +0200

    :triangular_ruler: Improves edit view animations (#76)

--- teferi/Views/EditTimeSlotView.swift
+++ teferi/Views/EditTimeSlotView.swift
@@ -5,6 +5,7 @@ class EditTimeSlotView : UIView
     //MARK: Fields
     private var editButtons : [UIImageView]? = nil
     private let onEditEnded : (TimeSlot, Category) -> Void
+    private var firstImageView : UIImageView? = nil
     
     //MARK: Initializers
     init(frame: CGRect, editEndedCallback: @escaping (TimeSlot, Category) -> Void)
@@ -12,7 +13,7 @@ class EditTimeSlotView : UIView
         self.onEditEnded = editEndedCallback
         super.init(frame: frame)
         self.alpha = 0
-        self.backgroundColor = UIColor.white.withAlphaComponent(0.6)
+        self.backgroundColor = UIColor.white.withAlphaComponent(0)
     }
     
     required init?(coder aDecoder: NSCoder)
@@ -30,10 +31,17 @@ class EditTimeSlotView : UIView
             
             var animationDelay = Double(viewsToRemove.count - 1) * Constants.editAnimationDuration
             
-            UIView.animate(withDuration: Constants.editAnimationDuration,
-                           delay: animationDelay - Constants.editAnimationDuration,
-                           options: [],
-                           animations:  { self.alpha = 0 })
+            UIView.animate(withDuration: Constants.editAnimationDuration * 3,
+                           delay: animationDelay - Constants.editAnimationDuration * 3,
+                           options: .curveLinear,
+                           animations:  {
+                                self.backgroundColor = UIColor.white.withAlphaComponent(0)
+                                self.firstImageView!.alpha = 0
+                            },
+                           completion: { (_) in
+                                self.alpha = 0
+                                self.firstImageView!.removeFromSuperview()
+                            } )
             
             viewsToRemove.forEach { v in
                 
@@ -61,6 +69,7 @@ class EditTimeSlotView : UIView
         guard point.x != 0 && point.y != 0 else { return }
         
         self.alpha = 1
+        
         self.editButtons = Constants.categories
             .filter { c in c != .unknown && c != timeSlot.category }
             .map { category in return self.mapCategoryIntoView(category, timeSlot) }
@@ -69,6 +78,7 @@ class EditTimeSlotView : UIView
         firstImageView.backgroundColor = timeSlot.category.color
         firstImageView.layer.cornerRadius = 16
         firstImageView.contentMode = .center
+        firstImageView.alpha = 0
         
         self.addSubview(firstImageView)
         firstImageView.snp.makeConstraints { make in
@@ -76,6 +86,12 @@ class EditTimeSlotView : UIView
             make.height.equalTo(32)
             make.top.equalTo(point.y - 24)
             make.left.equalTo(point.x - 32)
+        }
+        
+        UIView.animate(withDuration: Constants.editAnimationDuration * 3)
+        {
+            self.backgroundColor = UIColor.white.withAlphaComponent(0.6)
+            firstImageView.alpha = 1
         }
         
         var animationDelay = 0.0
@@ -103,7 +119,7 @@ class EditTimeSlotView : UIView
             previousImageView = imageView
         }
         
-        self.editButtons!.insert(firstImageView, at: 0)
+        self.firstImageView = firstImageView
     }
     
     private func mapCategoryIntoView(_ category: Category, _ timeSlot: TimeSlot) -> UIImageView
